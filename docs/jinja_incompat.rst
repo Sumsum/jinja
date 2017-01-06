c48ab368a78b505491c6cba8b70e2c262901bd54
Mikko Hellsing <mikko@aino.se>
2017-01-06 19:05:48 +0100

    adding some missing files

---
+++ docs/jinja_incompat.rst
@@ -0,0 +1,6 @@
+Filters
+-------
+- join, default join character is " " and not empty string
+- map, mosty works as before except that it does not call filters with first
+  argument but always treats that as the attribute argument.
+
---
+++ jinja2/liquid/datastructures.py
@@ -0,0 +1,23 @@
+from collections import UserList
+
+
+class List(UserList):
+    """
+    list with some exta (ruby) properties
+    """
+    def __init__(self, data=None):
+        self.data = list(data or [])
+
+    def __str__(self):
+        return ''.join([str(v) for v in self.data])
+
+    @property
+    def size(self):
+        return self.__len__()
+
+    @property
+    def first(self):
+        try:
+            return self.data[0]
+        except IndexError:
+            return None
